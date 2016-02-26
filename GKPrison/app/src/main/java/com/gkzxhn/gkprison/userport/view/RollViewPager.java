e41e973d80996aac6d21db60aed8ed5a11de7070
hzn <943852572@qq.com>
2016-02-26 18:31:25 +0800

    committed by 黄正能 on 2016/2/26 16:44

--- GKPrison/app/src/main/java/com/gkzxhn/gkprison/userport/view/RollViewPager.java
+++ GKPrison/app/src/main/java/com/gkzxhn/gkprison/userport/view/RollViewPager.java
@@ -178,14 +178,6 @@ public class RollViewPager extends ViewPager {
 		this.imgUrlList = imgUrlList;
 	}
 
-//	/**
-//	 * 此处测试用drawable下的图
-//	 * @param imgUrlList
-//	 */
-//	public void initImgUrl(int imgUrlList) {
-//		this.imgUrlList = imgUrlList;
-//	}
-
 	public void startRoll() {
 		// 1.给viewpager设置数据适配器
 		if (myPagerAdapter == null) {
@@ -217,8 +209,8 @@ public class RollViewPager extends ViewPager {
 			ImageView image = (ImageView) view.findViewById(R.id.image);
 			// 给imageview设置网络上获取的图片(三级缓存)
 			// 参数：第一个下载后要在哪个控件显示，第二个下载图片的链接地址
-			bitmapUtils.display(image, imgUrlList.get(position));
-//			image.setImageResource(CAROUSEL_IVS[position]);
+//			bitmapUtils.display(image, imgUrlList.get(position));
+			image.setImageResource(R.drawable.banner);
 			// viewpager和内部view的事件分发的过程
 			// 1.点下操作ACTION_DOWN先传递给viewpager，然后传递给viewpager内部的view，view做响应
 			// 2.滑动触发ACTION_MOVE事件，先传递给viewpager，然后传递给viewpager内部的view，view做响应
